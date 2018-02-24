8add453b6d05b62a6618b4744440e673aea242aa
Jordan Ball <jmball2016@gmail.com>
2018-02-24 12:02:34 -0500

    bug fix

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
   // Create a HuffmanTree object and read the input messages into the
   // HuffmanTree construct function. Next, print the encoded message.
   // Finally, destruct the huffman tree and move on to the next test case.
-	MinHeap<int> heap;
+	MinHeap<string> heap;
 	heap.insert("Hello,", 1);
 	heap.insert("my", 2);
 	heap.insert("name", 3);
