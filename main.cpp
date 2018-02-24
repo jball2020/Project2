96d149063b3089cb24c9b3eaf83f68eb002b06b6
Jordan Ball <jmball2016@gmail.com>
2018-02-24 11:59:47 -0500

    added more testing to main

--- main.cpp
+++ main.cpp
@@ -9,14 +9,25 @@ int main(int argc, char** argv) {
   // HuffmanTree construct function. Next, print the encoded message.
   // Finally, destruct the huffman tree and move on to the next test case.
 	MinHeap<int> heap;
-	heap.insert(3, 1);
-	heap.insert(4, 2);
-	heap.insert(5, 3);
+	heap.insert("Hello,", 1);
+	heap.insert("my", 2);
+	heap.insert("name", 3);
+	heap.insert("is", 4);
+	heap.insert("Jordan", 5);
+	heap.insert("Ball", 6);
 
-	int i = heap.peek();
+	string i = heap.peek();
 	cout << "min:" << i << endl;
 
 	heap.print();
+	cout << endl;
+
+	string e_i = heap.extract_min();
+	cout << "emin: " << e_i << endl;
+
+	heap.print();
+	cout << endl;
+
 
 	return(0);
 }
