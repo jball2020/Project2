4e027d0cf7d190b69e1ba3668e70956c376c725a
Jordan Ball <jmball2016@gmail.com>
2018-02-25 12:47:40 -0500

    filled in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,4 +6,15 @@ int main(int argc, char** argv) {
   // Create a HuffmanTree object and read the input messages into the
   // HuffmanTree construct function. Next, print the encoded message.
   // Finally, destruct the huffman tree and move on to the next test case.
+
+	int num_cases;
+	cin >> num_cases;
+	string message;
+	for (int i = num_cases; i > 0; i--) {
+		HuffmanTree mytree;
+		getline(cin, message);
+		mytree.construct(message);
+		mytree.print();
+	}
+
 }
